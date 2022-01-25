a27245a970c7b3245568c7b8f91810a83288e263
Alexander Entinger <cto@lxrobotics.com>
2022-01-25 08:34:35 +0100

    Turn lvgl gfx functions into private.

--- src/Braccio++.h
+++ src/Braccio++.h
@@ -78,11 +78,6 @@ protected:
   // io_expander APIs
   void digitalWrite(int pin, uint8_t value);
 
-  // default display APIs
-  void lvgl_splashScreen(unsigned long const duration_ms, std::function<void()> check_power_func);
-  void lvgl_pleaseConnectPower();
-  void lvgl_defaultMenu();
-
   void setID(int id) {
     servos.setID(id);
   }
@@ -122,6 +117,9 @@ private:
   lv_style_t _lv_style;
   rtos::Thread _display_thd;
   void display_thread_func();
+  void lvgl_splashScreen(unsigned long const duration_ms, std::function<void()> check_power_func);
+  void lvgl_pleaseConnectPower();
+  void lvgl_defaultMenu();
 
 #ifdef __MBED__
   rtos::EventFlags pd_events;
